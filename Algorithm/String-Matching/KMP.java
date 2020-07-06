b77532cf7fb63848a59962617cf0442c420801ec
najimali <najim.ali32@gmail.com>
2020-07-06 10:45:45 +0530

    Sample test case Added

--- Algorithm/String-Matching/KMP.java
+++ Algorithm/String-Matching/KMP.java
@@ -1,9 +1,21 @@
+
+// The Idea is to use Proper Prefix. First will compute the  longest prefix repeating(LPR) the apply KMP algo.
+// Sample test case
+// Input
+// 1
+// abaababa 
+// ab
+// Ouptut 
+// [0, 0]<- LRP array
+// Index is at 0
+// Index is at 3
+// Index is at 5
 import java.io.*;
 import java.util.*;
 
 class KMP {
     private static BufferedReader br;
-    private static StringTokenizer st;
+	private static StringTokenizer st;
 	public static void main (String[] args) throws Exception{
 		br = new BufferedReader(new InputStreamReader(System.in));
 		rl();
@@ -19,19 +31,19 @@ class KMP {
 	static void findIndexMathcing(char t[],char p[]){
 	    int m = p.length;
 	    int n = t.length;
-	    int lrp[] = computeP(p,m);
+	    int LPR[] = computeP(p,m);
 	    int k = -1;
 	
 	   for(int i=0;i<n;i++){
 	        
 	       while( k>-1 &&p[k+1]!=t[i]){
-	           k=lrp[k]-1;
+	           k=LPR[k]-1;
 	       }
 	       if(p[k+1]==t[i])
 	       k++;
 	       if(k+1==m){
 	           System.out.println("Index is at "+(i-m+1));
-	           k=lrp[k]-1;
+	           k=LPR[k]-1;
 	       }
 	       
 	    
@@ -40,20 +52,20 @@ class KMP {
 	}
 	static int[] computeP(char p[],int m){
 	    
-	    int lrp[] = new int[m];
+	    int LPR[] = new int[m];
 	    int k = -1;
-	    lrp[0] =0;
+	    LPR[0] =0;
 	    for(int i=1;i<m;i++){
 	        
 	        while( k>-1 &&p[k+1]!=p[i]){
-	            k=lrp[k]-1;
+	            k=LPR[k]-1;
 	        }
 	        if(p[k+1]==p[i])
 	        k++;
-	        lrp[i]=k+1;
+	        LPR[i]=k+1;
 	    }
-	    System.out.println(Arrays.toString(lrp));
-	    return lrp;
+	    System.out.println(Arrays.toString(LPR));
+	    return LPR;
 	}
     private static void rl() throws Exception{
 		st = new StringTokenizer(br.readLine());
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 #THIS REPO CONTAINS ALL THE ALGO WHICH I HAVE LEARN SO FAR & WILL BE LEARNING
+
+
 I will also upload interview question of differents companies & share my interview experience also.
